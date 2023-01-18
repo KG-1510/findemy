e5c664524d4d17ceab567fe3279f279e84c774b8
Kushagra Gupta <guptakushagra15.10@gmail.com>
2023-01-18 23:42:28 +0530

    ⚡️ Add search in mylearning page

--- src/components/mylearnings/mylearnings.tsx
+++ src/components/mylearnings/mylearnings.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
+import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { getUserProfile } from "../../utils/api";
-import { Checkoutordersloader } from "../checkout";
 import { Coursecardloader } from "../homepage";
 import {
   CoursecardComponent,
@@ -13,6 +13,7 @@ import {
 const MylearningsComponent = (): JSX.Element => {
   const [cookie, _] = useCookies(["authToken"]);
   const [purchasedCardsData, setPurchasedCardsData] = useState<any>([]);
+  const [purchasedCardsDataCopy, setPurchasedCardsDataCopy] = useState<any>([]);
   const [purchasedCardsLoaded, setPurchasedCardsLoaded] =
     useState<boolean>(false);
 
@@ -20,6 +21,7 @@ const MylearningsComponent = (): JSX.Element => {
     const _res = await getUserProfile(authToken, userId);
     if (_res) {
       setPurchasedCardsData(_res?.data?.coursesEnrolled);
+      setPurchasedCardsDataCopy(_res?.data?.coursesEnrolled);
       setPurchasedCardsLoaded(true);
     }
   };
@@ -31,6 +33,18 @@ const MylearningsComponent = (): JSX.Element => {
       fetchPurchasedItems(cookie?.authToken, _data?._id);
     }
   }, []);
+
+  const handleSearch = (e) => {
+    if (e.target.value !== "") {
+      console.log(e.target.value);
+      const searchedItems = purchasedCardsDataCopy.filter((item) => {
+        return item.title.toLowerCase().includes(e.target.value.toLowerCase());
+      });
+      setPurchasedCardsData(searchedItems);
+    } else if (e.target.value === "") {
+      setPurchasedCardsData(purchasedCardsDataCopy);
+    }
+  };
   return (
     <>
       <NavbarComponent />
@@ -40,7 +54,20 @@ const MylearningsComponent = (): JSX.Element => {
           Here is the list of all the courses you are currently enrolled in
         </p>
       </h1>
-      <div className="py-10 px-4 lg:px-44 font-bold">
+      <div className="pt-4 pb-14 px-4 lg:px-44 font-bold">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="bg-[#f8fafb] flex w-full lg:w-2/3 border border-primaryblack rounded-3xl py-5 flex-1 h-10 mt-2 mb-4 items-center"
+        >
+          <AiOutlineSearch className="h-5 mx-4 text-gray-400" />
+          <input
+            type="search"
+            onKeyUp={(e) => handleSearch(e)}
+            aria-label="Search"
+            placeholder="Search for anything"
+            className="bg-transparent text-sm font-normal outline-none p-2 w-full"
+          />
+        </form>
         {purchasedCardsData?.length === 0 && purchasedCardsData && (
           <div className="flex flex-col w-full items-center justify-center my-10">
             <h1 className="text-4xl">🫤</h1>
